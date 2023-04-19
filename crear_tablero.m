673b76b366dffdb33bf88e11bf032fa61b8666a5
Sebastian Oscar Lopez <sebastianoscarlopez@gmail.com>
2023-04-19 13:14:49 -0300

    nivel dificultad del tablero

--- crear_tablero.m
+++ crear_tablero.m
@@ -1,13 +1,16 @@
 % Empieza creando un tablero ganador, luego lo desordena con movimientos aleatorios
 function matriz = crear_tablero()
   n = 0;
-  while n < 3 || n > 9 % tamaño del tablero entre 3 y 9, sino se vuelve a pedir
-    disp("¿Tamaño del tablero entre 3 y 9? ");
+  disp("¿Tamaño de dificultad del tablero");
+  disp("    1 (fácil 3x3)");
+  disp("    2 (moderado 4x4)");
+  disp("    3 (profesional 5x5)");
+  while n < 1 || n > 3 % tamaño del tablero entre 3 y 9, sino se vuelve a pedir
     n = kbhit(); % lee el número, pero como texto
     n = str2num(n); % convierte a número
   end
 
-  matriz = crear_tablero_ganador(n);
+  matriz = crear_tablero_ganador(n + 2);
   for i = 1:1000 % 1000 movimientos aleatorios
     opcion = randi(4); % genera un número aleatorio entre 1 y 4
     switch opcion
